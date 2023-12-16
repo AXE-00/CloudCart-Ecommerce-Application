494f34168552b3148c90c9eae302a116256491ae
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-16 23:42:34 +0530

    created UserService microservice

---
+++ UserService/src/test/java/com_selfProject_UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com_selfProject_UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
