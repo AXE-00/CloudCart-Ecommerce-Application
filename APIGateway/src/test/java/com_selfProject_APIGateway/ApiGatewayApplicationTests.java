f3f2527aa608674146e26f9a4b075c592d963a42
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-16 23:38:27 +0530

    created APIGateway microservice

---
+++ APIGateway/src/test/java/com_selfProject_APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com_selfProject_APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
