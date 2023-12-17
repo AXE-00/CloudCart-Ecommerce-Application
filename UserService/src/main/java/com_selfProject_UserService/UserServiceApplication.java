7c0b5d784d8ca1fab54e997a96b0317c66f3d405
Aman Pandey <amanp.jun1906@gmail.com>
2023-12-18 00:54:41 +0530

    added filter class and bean in userService

--- UserService/src/main/java/com_selfProject_UserService/UserServiceApplication.java
+++ UserService/src/main/java/com_selfProject_UserService/UserServiceApplication.java
@@ -1,13 +1,28 @@
 package com_selfProject_UserService;
 
+import com_selfProject_UserService.filter.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<?> interUrl(){
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(new AuthFilter());
+		filterRegistrationBean.addUrlPatterns(
+				"/api/v1/userService/update/user"
+		);
+		return filterRegistrationBean;
 	}
 
 }
