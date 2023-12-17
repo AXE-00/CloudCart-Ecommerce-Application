ca499c5db2da83e64b228911f6af80c458f6cf2c
Aman Pandey <amanp.jun1906@gmail.com>
2023-12-17 23:50:42 +0530

    changes in user service

--- UserService/src/main/java/com_selfProject_UserService/repository/UserRepo.java
+++ UserService/src/main/java/com_selfProject_UserService/repository/UserRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface userRepo extends MongoRepository<User,String> {
+public interface UserRepo extends MongoRepository<User,String> {
 }
