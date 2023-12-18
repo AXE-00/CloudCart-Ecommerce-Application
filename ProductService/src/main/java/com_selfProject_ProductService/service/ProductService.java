f7f47b4ecdf60e533a23a436c2d014a501c8ad97
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-18 17:22:59 +0530

    threw exceptions to methods

--- ProductService/src/main/java/com_selfProject_ProductService/service/ProductService.java
+++ ProductService/src/main/java/com_selfProject_ProductService/service/ProductService.java
@@ -1,18 +1,19 @@
 package com_selfProject_ProductService.service;
 
 import com_selfProject_ProductService.domain.Product;
+import com_selfProject_ProductService.exception.ProductAlreadyExistsException;
 import com_selfProject_ProductService.exception.ProductNotFoundException;
 
 import java.util.List;
 
 public interface ProductService {
-    Product addProducts(Product product, int productId) throws ProductNotFoundException;
+    Product addProducts(Product product) throws ProductAlreadyExistsException;
 
-    List<Product> getAllProducts();
+    List<Product> getAllProducts() throws ProductNotFoundException;
 
-    Product getByName(String productName);
+    Product getByName(String productName) throws ProductNotFoundException;
 
-    Product updateProduct(Product product, int productId);
+    Product updateProduct(Product product, int productId) throws ProductNotFoundException;
 
-    boolean deleteById(int productId);
+    boolean deleteById(int productId) throws ProductNotFoundException;
 }
