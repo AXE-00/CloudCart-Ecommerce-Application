00b9b7b943f5ee5cdfac1781e33a7a4308bd3ab6
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-18 17:03:32 +0530

    extended MongoRepository to ProductRepository

--- ProductService/src/main/java/com_selfProject_ProductService/repository/ProductRepository.java
+++ ProductService/src/main/java/com_selfProject_ProductService/repository/ProductRepository.java
@@ -1,9 +1,10 @@
 package com_selfProject_ProductService.repository;
 
 import com_selfProject_ProductService.domain.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository {
+public interface ProductRepository extends MongoRepository<Product, Integer> {
     Product findByProductName(String productName);
 }
