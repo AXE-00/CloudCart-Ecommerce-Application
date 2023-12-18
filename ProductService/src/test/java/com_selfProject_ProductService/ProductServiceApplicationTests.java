fa5d72327deb0b4ca62fd23ec0c504311cea9baa
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-18 16:40:03 +0530

    added product service

---
+++ ProductService/src/test/java/com_selfProject_ProductService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com_selfProject_ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
