a64487fa37aa1981257a0f3422bdc61701424599
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-18 16:58:12 +0530

    added addProducts() in ProductService.java

--- ProductService/src/main/java/com_selfProject_ProductService/service/ProductService.java
+++ ProductService/src/main/java/com_selfProject_ProductService/service/ProductService.java
@@ -1,4 +1,9 @@
 package com_selfProject_ProductService.service;
 
+import com_selfProject_ProductService.domain.Product;
+import com_selfProject_ProductService.exception.ProductNotFoundException;
+
 public interface ProductService {
+    Product addProducts(Product product, int productId) throws ProductNotFoundException;
+
 }
