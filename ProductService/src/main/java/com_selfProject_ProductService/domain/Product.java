36daf6f06f99654e75e7a5c813c24c5690acda22
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-18 16:49:00 +0530

    added annotations in Product.java

--- ProductService/src/main/java/com_selfProject_ProductService/domain/Product.java
+++ ProductService/src/main/java/com_selfProject_ProductService/domain/Product.java
@@ -1,4 +1,15 @@
 package com_selfProject_ProductService.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document
 public class Product {
+
 }
