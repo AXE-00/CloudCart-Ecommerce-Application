118a4607734bb3d88dff82e112a2d7ecbb20d1b3
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-18 17:22:02 +0530

    added constructor to ProductNotFoundException.java

--- ProductService/src/main/java/com_selfProject_ProductService/exception/ProductNotFoundException.java
+++ ProductService/src/main/java/com_selfProject_ProductService/exception/ProductNotFoundException.java
@@ -5,4 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Product with this Id does not exist in the database")
 public class ProductNotFoundException extends Exception {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
 }
