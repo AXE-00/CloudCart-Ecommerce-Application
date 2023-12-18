ca601e578cc95a68e5fe321914f278c9fbf13b44
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-18 16:54:15 +0530

    added findByProductName() in ProductRepository.java

--- ProductService/src/main/java/com_selfProject_ProductService/repository/ProductRepository.java
+++ ProductService/src/main/java/com_selfProject_ProductService/repository/ProductRepository.java
@@ -1,8 +1,9 @@
 package com_selfProject_ProductService.repository;
 
+import com_selfProject_ProductService.domain.Product;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository {
-
+    Product findByProductName(String productName);
 }
