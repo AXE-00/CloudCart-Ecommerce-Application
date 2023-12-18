3691a65e97e4d280d273d72170e0187c10e96408
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-18 17:25:30 +0530

    completed addProducts() in ProductServiceImpl.java

--- ProductService/src/main/java/com_selfProject_ProductService/service/ProductServiceImpl.java
+++ ProductService/src/main/java/com_selfProject_ProductService/service/ProductServiceImpl.java
@@ -1,12 +1,14 @@
 package com_selfProject_ProductService.service;
 
 import com_selfProject_ProductService.domain.Product;
+import com_selfProject_ProductService.exception.ProductAlreadyExistsException;
 import com_selfProject_ProductService.exception.ProductNotFoundException;
 import com_selfProject_ProductService.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -16,27 +18,32 @@ public class ProductServiceImpl implements ProductService {
     private ProductRepository productRepository;
 
     @Override
-    public Product addProducts(Product product, int productId) throws ProductNotFoundException {
+    public Product addProducts(Product product) throws ProductAlreadyExistsException {
+        int productId = product.getProductId();
+        Optional<Product> optionalProduct = this.productRepository.findById(productId);
+        if (optionalProduct.isPresent()) {
+            throw new ProductAlreadyExistsException("Product with this Id already exists");
+        }
+        return this.productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> getAllProducts() throws ProductNotFoundException {
         return null;
     }
 
     @Override
-    public List<Product> getAllProducts() {
+    public Product getByName(String productName) throws ProductNotFoundException {
         return null;
     }
 
     @Override
-    public Product getByName(String productName) {
+    public Product updateProduct(Product product, int productId) throws ProductNotFoundException {
         return null;
     }
 
     @Override
-    public Product updateProduct(Product product, int productId) {
-        return null;
-    }
-
-    @Override
-    public boolean deleteById(int productId) {
+    public boolean deleteById(int productId) throws ProductNotFoundException {
         return false;
     }
 }
