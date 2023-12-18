f346bf43145ad0eaf5ff1a76e1cf6670e2e95d6a
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-18 17:02:09 +0530

    added @Service annotation and injected field dependency in ProductServiceImpl.java

--- ProductService/src/main/java/com_selfProject_ProductService/service/ProductServiceImpl.java
+++ ProductService/src/main/java/com_selfProject_ProductService/service/ProductServiceImpl.java
@@ -2,11 +2,19 @@ package com_selfProject_ProductService.service;
 
 import com_selfProject_ProductService.domain.Product;
 import com_selfProject_ProductService.exception.ProductNotFoundException;
+import com_selfProject_ProductService.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
+@Service
 public class ProductServiceImpl implements ProductService {
+
+
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public Product addProducts(Product product, int productId) throws ProductNotFoundException {
         return null;
