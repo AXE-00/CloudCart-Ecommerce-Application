e90a452e69abeadcd74e8aabf7c41b622737ef64
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-18 16:59:18 +0530

    added getAllProducts() and getByName() in ProductService.java

--- ProductService/src/main/java/com_selfProject_ProductService/service/ProductService.java
+++ ProductService/src/main/java/com_selfProject_ProductService/service/ProductService.java
@@ -3,7 +3,13 @@ package com_selfProject_ProductService.service;
 import com_selfProject_ProductService.domain.Product;
 import com_selfProject_ProductService.exception.ProductNotFoundException;
 
+import java.util.List;
+
 public interface ProductService {
     Product addProducts(Product product, int productId) throws ProductNotFoundException;
+
+    List<Product> getAllProducts();
+
+    Product getByName(String productName);
 
 }
