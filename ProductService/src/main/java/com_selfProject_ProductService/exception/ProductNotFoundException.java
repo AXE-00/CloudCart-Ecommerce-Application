bd771f5a6b07d3add51ec1512eca5f5a77128f54
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-18 16:57:39 +0530

    created ProductNotFoundException.java; added @ResponseStatus annotation

---
+++ ProductService/src/main/java/com_selfProject_ProductService/exception/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package com_selfProject_ProductService.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Product with this Id does not exist in the database")
+public class ProductNotFoundException extends Exception {
+}
