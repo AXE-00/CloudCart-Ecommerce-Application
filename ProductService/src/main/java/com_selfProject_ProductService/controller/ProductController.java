d03895a40c58d5536f3dea0ce1f703aa010e6c23
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-18 17:57:28 +0530

    created ProductController.java; added @Controller and @RequestMapping annotations

---
+++ ProductService/src/main/java/com_selfProject_ProductService/controller/ProductController.java
@@ -0,0 +1,10 @@
+package com_selfProject_ProductService.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/v1/product")
+public class ProductController {
+
+}
