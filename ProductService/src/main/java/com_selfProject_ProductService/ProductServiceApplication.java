6e87e8604a3471b66056d70b39ae77953cdb4df8
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-20 09:33:10 +0530

    added filterRegistrationBean() in ProductServiceApplication.java

--- ProductService/src/main/java/com_selfProject_ProductService/ProductServiceApplication.java
+++ ProductService/src/main/java/com_selfProject_ProductService/ProductServiceApplication.java
@@ -1,8 +1,11 @@
 package com_selfProject_ProductService;
 
+import com_selfProject_ProductService.filter.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +14,14 @@ public class ProductServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
+	@Bean
+	public FilterRegistrationBean filterRegistrationBean() {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean<>();
+		filterRegistrationBean.setFilter(new JwtFilter());
 
+		filterRegistrationBean.addUrlPatterns("/api/v1/product/addNewProduct",
+				"/api/v1/product/update/*",
+				"/api/v1/product/delete/*");
+		return filterRegistrationBean;
+	}
 }
