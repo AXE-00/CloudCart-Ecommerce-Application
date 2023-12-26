2ca0c3bfc250704950b769845e8ef7c3f1fbe157
Ashutosh <ashutosh.k.work@gmail.com>
2023-12-26 15:24:32 +0530

    added app.module.ts

--- CloudCart-Frontend-Application/src/app/app.component.ts
+++ CloudCart-Frontend-Application/src/app/app.component.ts
@@ -4,8 +4,6 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
