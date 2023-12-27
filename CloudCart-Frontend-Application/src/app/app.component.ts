7b2bd424b99cf55c3ced8354ee3707cc80721674
Aman Pandey <amanp.jun1906@gmail.com>
2023-12-27 16:05:38 +0530

    Removed module.ts

--- CloudCart-Frontend-Application/src/app/app.component.ts
+++ CloudCart-Frontend-Application/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, NavBarComponent]
 })
 export class AppComponent {
   title = 'CloudCart';
--- CloudCart-Frontend-Application/src/app/app.routes.ts
+++ CloudCart-Frontend-Application/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+       path:"home",
+       component:NavBarComponent 
+    },{
+
+    }
+];
