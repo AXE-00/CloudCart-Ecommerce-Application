af6e37cfe07f4f9303a8be6cc117934e53f92358
Aman Pandey <amanp.jun1906@gmail.com>
2024-01-01 00:37:02 +0530

    Added Footer

--- CloudCart-Frontend-Application/src/app/app.component.ts
+++ CloudCart-Frontend-Application/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavBarComponent]
+    imports: [CommonModule, RouterOutlet, NavBarComponent,FooterComponent]
 })
 export class AppComponent {
   title = 'CloudCart';
