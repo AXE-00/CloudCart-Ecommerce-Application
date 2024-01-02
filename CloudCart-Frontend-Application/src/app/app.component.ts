3a0984ba198558de245534febe7ca6493cc321e9
Ashutosh <ashutosh.k.work@gmail.com>
2024-01-02 15:59:19 +0530

    imported HttpClientModule

--- CloudCart-Frontend-Application/src/app/app.component.ts
+++ CloudCart-Frontend-Application/src/app/app.component.ts
@@ -5,13 +5,14 @@ import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { FooterComponent } from './footer/footer.component';
 import { MainFrontComponent } from "./main-front/main-front.component";
 import { LoginComponent } from "./login/login.component";
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavBarComponent, FooterComponent, MainFrontComponent, LoginComponent]
+    imports: [CommonModule, RouterOutlet, NavBarComponent, FooterComponent, MainFrontComponent, LoginComponent, HttpClientModule]
 })
 export class AppComponent {
   title = 'CloudCart';
