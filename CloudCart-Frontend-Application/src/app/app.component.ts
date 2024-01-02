a44dc47c02ef09813b79f58258d7bb721dfb792e
Aman Pandey <amanp.jun1906@gmail.com>
2024-01-02 12:32:35 +0530

    app component

--- CloudCart-Frontend-Application/src/app/app.component.ts
+++ CloudCart-Frontend-Application/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { FooterComponent } from './footer/footer.component';
+import { MainFrontComponent } from "./main-front/main-front.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavBarComponent,FooterComponent]
+    imports: [CommonModule, RouterOutlet, NavBarComponent, FooterComponent, MainFrontComponent]
 })
 export class AppComponent {
   title = 'CloudCart';
