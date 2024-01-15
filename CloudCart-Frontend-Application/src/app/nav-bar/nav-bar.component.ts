e99b9cf00cf3b398c32be4b012e6d8c7fedf7e49
Aman Pandey <amanp.jun1906@gmail.com>
2024-01-15 18:08:49 +0530

    added routerLink

--- CloudCart-Frontend-Application/src/app/nav-bar/nav-bar.component.ts
+++ CloudCart-Frontend-Application/src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+constructor(){}
+
 
 }
