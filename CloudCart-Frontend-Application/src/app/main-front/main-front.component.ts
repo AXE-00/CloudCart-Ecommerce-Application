e785b3663363c587f0d4458280a0cb83bfbb718a
Aman Pandey <amanp.jun1906@gmail.com>
2024-01-19 03:24:12 +0530

    ...

--- CloudCart-Frontend-Application/src/app/main-front/main-front.component.ts
+++ CloudCart-Frontend-Application/src/app/main-front/main-front.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "../nav-bar/nav-bar.component";
 
 @Component({
-  selector: 'app-main-front',
-  standalone: true,
-  imports: [],
-  templateUrl: './main-front.component.html',
-  styleUrl: './main-front.component.css'
+    selector: 'app-main-front',
+    standalone: true,
+    imports: [],
+    templateUrl: './main-front.component.html',
+    styleUrl: './main-front.component.css'
+    
 })
 export class MainFrontComponent {
 
