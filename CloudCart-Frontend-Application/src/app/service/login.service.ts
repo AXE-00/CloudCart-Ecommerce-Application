007d22d7ec1160037aeeba8b5f17699ed5f88a1f
Aman Pandey <amanp.jun1906@gmail.com>
2024-01-19 03:21:51 +0530

    added event emitter

--- CloudCart-Frontend-Application/src/app/service/login.service.ts
+++ CloudCart-Frontend-Application/src/app/service/login.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
 @Injectable({
@@ -6,13 +6,30 @@ import {HttpClient} from "@angular/common/http";
 })
 export class LoginService {
 
-	authUrl: String = "http://localhost:9000/api/v1/authService"
+	isLoggedIn:boolean=false;
+
+	userLoggedIn = new EventEmitter<any>();
+
+	authUrl: String = "http://localhost:9000/api/v1/authService";
 
 	constructor(private httpClient: HttpClient) {
 	}
 
+	loginSuccess(){
+		this.isLoggedIn=true;
+		this.userLoggedIn.emit(true);
+	}
+
+	loginfailure(){
+		this.isLoggedIn=false;
+	}
+
+	getLoginStatus(){
+        return this.isLoggedIn;
+	}
+
 	login(loginData: any) {
-    console.log(loginData);
+    // console.log(loginData);
 		return this.httpClient.post(`${this.authUrl}/user/login`, loginData);
 	}
 }
