602a6ea6bdaf2c97eed63a1a529e749f3e12f05d
Aman Pandey <amanp.jun1906@gmail.com>
2024-01-19 03:22:37 +0530

    user name and login button

--- CloudCart-Frontend-Application/src/app/nav-bar/nav-bar.component.ts
+++ CloudCart-Frontend-Application/src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit} from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { LoginService } from '../service/login.service';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -8,8 +10,34 @@ import { RouterModule } from '@angular/router';
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
-constructor(){}
+export class NavBarComponent implements OnInit{
+  
+userName:any;
+loginTime:any="Login";
 
+constructor(private logService:LoginService,private userSer:UserService, private router :Router){}
+  ngOnInit(): void {
+    
+    this.userName ="User" ;
 
+    this.logService.userLoggedIn.subscribe(()=>{
+
+      this.userName = localStorage.getItem('name');
+        this.loginTime="LogOut";
+
+    })
+  }
+
+  clicked(){
+    if(this.loginTime==='Login'){
+      this.router.navigateByUrl('/login');
+    }
+    if(this.loginTime === 'LogOut'){
+      this.logService.loginfailure();
+      localStorage.clear();
+      this.router.navigateByUrl("/login")
+      this.loginTime='Login';
+      this.userName ="User" ;
+    }
+  }
 }
