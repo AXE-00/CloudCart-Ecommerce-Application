a4d543c2012c1b823598af09bf7189ac4682f8d8
Aman Pandey <amanp.jun1906@gmail.com>
2024-01-20 17:26:33 +0530

    supplier dashboard component

---
+++ CloudCart-Frontend-Application/src/app/supplier-dashboard/supplier-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supplier-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './supplier-dashboard.component.html',
+  styleUrl: './supplier-dashboard.component.css'
+})
+export class SupplierDashboardComponent {
+
+}
