d6d98ce996df20e583104425b2f2d82337c265ca
Aman Pandey <amanp.jun1906@gmail.com>
2024-01-24 23:45:10 +0530

    added product dash

--- CloudCart-Frontend-Application/src/app/main-front/main-front.component.ts
+++ CloudCart-Frontend-Application/src/app/main-front/main-front.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { ProductDashComponent } from "../product-dash/product-dash.component";
 
 @Component({
     selector: 'app-main-front',
     standalone: true,
-    imports: [],
     templateUrl: './main-front.component.html',
-    styleUrl: './main-front.component.css'
-    
+    styleUrl: './main-front.component.css',
+    imports: [ProductDashComponent]
 })
 export class MainFrontComponent {
 
