297807611c102eb54b77968f6cd4fd87dfe00830
Aman Pandey <amanp.jun1906@gmail.com>
2024-01-24 17:11:07 +0530

    email Services added

--- APIGateway/src/main/java/com_selfProject_APIGateway/APIGateWay.java
+++ APIGateway/src/main/java/com_selfProject_APIGateway/APIGateWay.java
@@ -20,6 +20,8 @@ public class APIGateWay {
                         .uri("http://localhost:8082"))
                 .route(p->p.path("/api/v1/productService/**")
                         .uri("http://localhost:8083"))
+                .route(p->p.path("/api/v1/emailService/**")
+                        .uri("http://localhost:8089"))
                 .build();
     }
 
