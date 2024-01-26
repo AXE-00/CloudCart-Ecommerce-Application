5e4ba892fdffcc2f1ec666d47d5d515f3325169e
Aman Pandey <amanp.jun1906@gmail.com>
2024-01-26 17:40:44 +0530

    product dashboard is added

--- CloudCart-Frontend-Application/src/app/product-dash/product-dash.component.ts
+++ CloudCart-Frontend-Application/src/app/product-dash/product-dash.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../service/product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-dash',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-dash.component.html',
   styleUrl: './product-dash.component.css'
 })
-export class ProductDashComponent {
+export class ProductDashComponent implements OnInit {
 
+
+  productsData:any=[];
+  productsLoaded:boolean=false;
+
+  constructor(private productService:ProductService){}
+
+  ngOnInit(): void {
+   this.fetchProducts();
+  }
+
+  fetchProducts(){
+    this.productService.getAllProduct().subscribe({
+      next:data=>{
+       this.productsData = data;
+       console.log(this.productsData);
+       this.productsLoaded=true;
+      }
+    })
+  }
 }
