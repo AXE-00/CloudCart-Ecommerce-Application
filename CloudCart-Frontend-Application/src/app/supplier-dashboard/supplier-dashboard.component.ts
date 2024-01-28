560e82d46998782f5aa738b5c1928737ff88555f
Aman Pandey <amanp.jun1906@gmail.com>
2024-01-28 19:10:30 +0530

    Supplier dashboard is added

--- CloudCart-Frontend-Application/src/app/supplier-dashboard/supplier-dashboard.component.ts
+++ CloudCart-Frontend-Application/src/app/supplier-dashboard/supplier-dashboard.component.ts
@@ -13,66 +13,9 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrl: './supplier-dashboard.component.css'
 })
 export class SupplierDashboardComponent implements OnInit {
-
-  userEmail:any='';
-  otpClicked:boolean=true;
-  verifyClicked:boolean=false;
-  supplierVerified:boolean=false;
-  timeLeft=60;
-  otpGet:any=0;
-  checkOtp:any=0;
-  constructor(private userSer:UserService, private _matSnack:MatSnackBar){}
-
   ngOnInit(): void {
-    this.userSer.getUserData().subscribe((data:any)=>{
-      this.userEmail=data.userEmail;
-    })
-  }
-
-  getOtp(){
-    this.userSer.getOtp(this.userEmail).subscribe({
-      next:data=>{
-        console.log(data);
-        this.checkOtp=data;
-        
-        this._matSnack.open(`OTP Sent Successfully..`, 'success', {
-          duration: 2000,
-          panelClass: ['mat-toolbar', 'mat-primary']
-        });
-        
-        this.otpClicked=false;
-        this.verifyClicked=true;
-      }
-    })
-
-    var countDownInterval = setInterval(()=>{
     
-      this.timeLeft--;
+  }
+
   
-      if(this.timeLeft == 0){
-
-        clearInterval(countDownInterval);
-        this.otpClicked=true;
-        this.verifyClicked=false;
-        this.timeLeft=60;
-      }
-     },1000) 
-  }
-
-  verifyOtp(){
-    if(this.otpGet==this.checkOtp){
-      this._matSnack.open(`Verification Completed..`, 'success', {
-        duration: 3000,
-        panelClass: ['mat-toolbar', 'mat-primary']
-      });
-     
-    }
-    else{
-      this._matSnack.open(`Entered OTP is Not Correct..`, 'Failure', {
-        duration: 3000,
-        panelClass: ['mat-toolbar', 'mat-primary']
-      });
-      this.timeLeft=60;
-    }
-  }
 }
