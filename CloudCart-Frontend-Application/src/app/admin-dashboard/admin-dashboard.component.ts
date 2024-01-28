156520a575532960356b65c3a91ff59e835e18db
Aman Pandey <amanp.jun1906@gmail.com>
2024-01-28 19:04:47 +0530

    admin Dashboard Added

--- CloudCart-Frontend-Application/src/app/admin-dashboard/admin-dashboard.component.ts
+++ CloudCart-Frontend-Application/src/app/admin-dashboard/admin-dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
